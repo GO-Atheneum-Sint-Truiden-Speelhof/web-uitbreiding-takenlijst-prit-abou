21d691e4733035211ab6b96e4397c3c33520a749
Prit12345678 <harpritamjit.singh@atheneumsinttruiden.be>
2024-11-18 14:23:19 +0100

    ik heb geprobeerd om de taken te posten

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang=nl>
+
+<head>
+	<meta charset=utf-8>
+	<meta name="robots" content="all">
+	<link rel="stylesheet" type="text/css" href="opmaak/bootstrap.css">
+	<title>To-Do Lijst</title>
+	<script src="scripts/jquery.js"></script>
+	<script src="scripts/bootstrap.js"></script>
+	<link rel="icon" type="image/png" href="favicon/lijstfoto.webp">
+	<h1 class="text-center mb-4">To-Do List</h1>
+</head>
+
+<body>
+<form method="post" action="post.php">
+        <div class="input-group">
+        <input type="text" id="task-name" class="form-control" required>
+        <button type="submit" class="btn btn-dark">Toevoegen</button>
+		
+        </div>
+        </form>
+
+        
+        
+	<?php include "includes/functies.php";?>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <h1 class="text-center mb-4">To-Do List</h1>
+</head>
+<body>
+
+    <?php
+
+     '.$_POST["task-name"].'
+     
+    ?>
+
+<div class="card shadow-sm">
+            <div class="card-body">
+            <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                 <th>Nr</th>
+                 <th>Taak</th>
+                 <th>Status</th>
+				 <th>Gemaakt op</th>
+                </tr>
+            </thead>
+
+            <tbody id="taak-lijst">
+				<?php 
+				$sql = "SELECT * FROM tasks";
+				$result = mysqli_query($sql);
+				?>
+            </tbody>
+            </table>
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
+
