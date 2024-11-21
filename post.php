76d7cfe5d0efb48b487fcaf48c7d50c6d927197d
Sabou18 <abou.sal@atheneumsinttruiden.be>
2024-11-21 11:12:41 +0100

    k

--- post.php
+++ post.php
@@ -31,7 +31,7 @@
             $password = "";            
             $dbname = "tasks"; 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
             if (!$conn) {
                 die("Connection failed: " . mysqli_connect_error());
