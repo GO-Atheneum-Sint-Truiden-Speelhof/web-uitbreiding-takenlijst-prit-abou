8fe75e237ebf8779fd4b4c4604610f32ecff279d
Sabou18 <abou.sal@atheneumsinttruiden.be>
2024-12-03 11:01:26 +0100

    .

--- post.php
+++ post.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <h1 class="text-center mb-4">To-Do List</h1>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
-
+    <h1 class="text-center mb-4">To-Do List</h1>
     <?php
    
      '.$_POST["task-name"].'
